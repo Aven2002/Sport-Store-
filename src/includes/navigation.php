c9e4a2044552c72339cfd208c136e4acbe37422b
AvenDing <avending2002@1utar.my>
2024-04-19 02:16:41 +0800

    Change nav bar styling

--- src/includes/navigation.php
+++ src/includes/navigation.php
@@ -1,71 +1,5 @@
-<style>
-  nav {
-    background-color: #333;
-  }
+<link rel="stylesheet" href="../../css/navBar.css?ver=<?php echo filemtime('../../css/navBar.css'); ?>" />
 
-  .navbar {
-    margin: 0 auto;
-    overflow: hidden;
-    width: max-content;
-  }
-
-  .navbar a {
-    float: left;
-    color: #fff;
-    text-decoration: none;
-    padding: 10px 15px;
-    border-radius: 5px;
-    transition: background-color 0.3s ease;
-  }
-
-  .nav {
-    float: left;
-    overflow: hidden;
-  }
-
-  .nav .navDropBtn {
-    border-radius: 5px;
-    border: none;
-    outline: none;
-    color: white;
-    padding: 10px 15px;
-    background-color: inherit;
-    font-family: inherit;
-    margin: 0;
-  }
-
-  .navbar a:hover,
-  .nav:hover .navDropBtn,
-  .nav-content {
-    background-color: #555;
-  }
-
-  .nav-content {
-    display: none;
-    position: absolute;
-    min-width: 160px;
-    color: var(--color-font);
-    z-index: 1;
-  }
-
-  .nav-content a {
-    float: none;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    border-radius: 0;
-    text-align: left;
-    text-transform: capitalize;
-  }
-
-  .nav-content a:hover {
-    background-color: var( --color-font-shadow);
-  }
-
-  .nav:hover .nav-content {
-    display: block;
-  }
-</style>
 <?php
 include("../../database.php");
 $sql = "SELECT DISTINCT productCategory FROM product";
@@ -84,6 +18,7 @@ echo "<script>console.log(cats);</script>";
 <nav>
   <div class="navbar">
     <a href="Home.php">Home</a>
+    <a href="Contact.php">Contact</a>
     <div class="nav">
       <div class="navDropBtn m-0">Products <?php if (!empty($cats)) echo '&#9662'; ?></div>
       <?php if (!empty($cats)) : ?>
@@ -94,6 +29,5 @@ echo "<script>console.log(cats);</script>";
       </div>
       <?php endif; ?>
     </div>
-    <a href="Contact.php">Contact</a>
   </div>
 </nav>
